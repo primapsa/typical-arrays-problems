afda985bfa7cdaa92aa692a1017e359a14a20534
Siarhei Prymakovich <prima.psa@gmail.com>
2021-03-10 23:08:49 +0300

    feat: implement MIN function

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 
-exports.min = function min (array) {
-  
+exports.min = function min (array) {  
+  if(!array || array.length < 1) return 0; 
+  let sorted = array.sort(function(a,b){ return a-b});
+  let sortedMin = sorted.shift();
+  return sortedMin;
 }
 
 exports.max = function max (array) {
