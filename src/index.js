28894f540bd4631c99bc23743bd1d51d80a81be9
Siarhei Prymakovich <prima.psa@gmail.com>
2021-03-10 23:14:40 +0300

    feat: implement MAX function

--- src/index.js
+++ src/index.js
@@ -7,7 +7,10 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || array.length < 1) return 0; 
+  let sorted = array.sort(function(a,b){ return a-b});
+  let sortedMax = sorted.pop();
+  return sortedMax;
 }
 
 exports.avg = function avg (array) {
